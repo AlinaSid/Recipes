2f9d5a8d0d9555569e7677d33cb4b25590682ebe
AlinaSid <alinanice@list.ru>
2024-02-06 22:12:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import video from './food.mp4'
+import MyRecipesComponent from './MyRecipesComponent';
 
 function App() {
+
+
+  const [MySearch,setMySearch]= useState("");
+  const [MyRecipes,setMyRecipes]=useState([]);
+
+  // const MY_ID ="af7a0d97";
+  // const MY_KEY = "%204e8696da3ba97e3073949460db305d40%09%E2%80%94";
+
+  useEffect(()=>{
+ const getRecipe = async ()=>{
+  const responce = await fetch ('https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=af7a0d97&app_key=%204e8696da3ba97e3073949460db305d40%09%E2%80%94');
+  const data= await responce.json();
+  setMyRecipes(data.hits)
+ }
+getRecipe()
+},[])
+
+
+const MyRecipSearch = (e)=> {
+console.log(e.target.value)
+setMySearch(e.target.value)
+
+
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <video autoPlay muted loop >
+        <source src={video} type='video/mp4'  />
+      </video>
+   <h1>Find a Recipe</h1>
+   </div>
+
+   <div className='container'>
+     <form>
+         <input className='search' placeholder='search...' onChange={MyRecipSearch} value={MySearch}/>
+    </form>
+</div>
+
+<div className='container'>
+     <button>
+         <img src="https://img.icons8.com/fluency/48/000000/fry.png" alt="icon"/>
+      </button>
+</div>
+
+{MyRecipes.map(element => (
+  <MyRecipesComponent/>
+))}
+
+
     </div>
   );
 }
---
+++ src/MyRecipesComponent.js
@@ -0,0 +1,14 @@
+ function MyRecipesComponent(){
+ return (
+<div>
+<p>TEST</p>
+
+</div>
+
+
+ )
+
+ }
+
+
+ export default MyRecipesComponent
